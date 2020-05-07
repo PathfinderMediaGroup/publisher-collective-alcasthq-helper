40b1c78bc15b4d1421b3a63208a2cfc04300aa9a
Georgia Ananieva <georgia.ananieva@network-n.com>
2020-05-07 12:56:13 +0100

    AlcastHQ: replaced os1.network-n.com with dash.os.network-n.com for Prebid Instream Test.

--- views/single-script-solution.php
+++ views/single-script-solution.php
@@ -2,6 +2,7 @@
 // Remove .com from domain name
 $domain = str_replace('.com', '', $this->domain);
 // Create the url dynamically using the correct wordpress path and domain name
-$url = sprintf('https://os1.network-n.com/dist/%s.min.js', $domain);
+$url = sprintf('https://dash.os.network-n.com/dist/%s.min.js', $domain);
 ?>
+
 <script async="async" src="<?php echo $url; ?>"></script>
