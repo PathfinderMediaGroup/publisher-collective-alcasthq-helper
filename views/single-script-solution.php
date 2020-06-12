a3001c5e50795838616beda0f245f04cc71db927
Tom Dyer <tom.dyer@network-n.com>
2020-06-12 17:38:00 +0100

    Changing domain for Single Script

--- views/single-script-solution.php
+++ views/single-script-solution.php
@@ -2,7 +2,7 @@
 // Remove .com from domain name
 $domain = str_replace('.com', '', $this->domain);
 // Create the url dynamically using the correct wordpress path and domain name
-$url = sprintf('https://os1.network-n.com/dist/%s.min.js', $domain);
+$url = sprintf('https://beta.network-n.com/dist/%s.min.js', $domain);
 ?>
 
 <script async="async" src="<?php echo $url; ?>"></script>
