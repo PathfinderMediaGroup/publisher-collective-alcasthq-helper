a14113de8bb05138894170160b69a3dc15ca623f
Jason Ashdown <jason@network-n.com>
2020-07-28 11:27:03 +0100

    Update version date

--- networkn-ad-helper.php
+++ networkn-ad-helper.php
@@ -3,7 +3,7 @@
 Plugin Name:  Network-N Advertisement Helper
 Plugin URI:   https://www.network-n.com/
 Description:  Network-N Ads scripts plugin for WordPress sites
-Version:      20190701
+Version:      20200728
 Author:       NETWORK N
 Author URI:   https://www.network-n.com/
 Text Domain:  networkn
