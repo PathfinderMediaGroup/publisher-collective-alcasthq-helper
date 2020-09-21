a8d4fddd17866ec9140f7005d257b11a0ada94f9
Jason Ashdown <jason@network-n.com>
2020-09-21 14:50:26 +0100

    Update script to be compatible with Patreon

--- views/single-script-solution.php
+++ views/single-script-solution.php
@@ -3,6 +3,13 @@
 $domain = str_replace('.com', '', $this->domain);
 // Create the url dynamically using the correct wordpress path and domain name
 $url = sprintf('https://os1.network-n.com/dist/%s.min.js', $domain);
-?>
 
-<script async="async" src="<?php echo $url; ?>"></script>
+if (class_exists('Patreon_Frontend') && method_exists('Patreon_Frontend', 'hide_ad_for_patrons')) {
+	$patreon = Patreon_Frontend::hide_ad_for_patrons(1, '<script async="async" src="'.$url.'"></script>');
+} else {
+	$patreon = '<script async="async" src="'.$url.'"></script>';
+}
+
+if (!empty($patreon)) {
+	echo $patreon;
+}
